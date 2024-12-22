acb6eb05b611fbd2d2fca995d9dd757393610d10
parthnain <parthn@uw.edu>
2024-12-21 21:44:57 -0800

    Updated base url in vite config

--- PokeDisplay/vite.config.js
+++ PokeDisplay/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/PokemonCards/', // Replace with your repository name
+  base: '/PokeDisplay/', // Replace with your repository name
   build: {
     outDir: 'dist',
   },
